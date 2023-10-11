fd94d39bb8c745fa6992cfa02d9b87806a3d93fc
Kaustuv Devmishra <117767351+kaustuv-d@users.noreply.github.com>
2023-10-11 17:59:34 +0530

    Create keypoints_MediaPipe.py

---
+++ keypoints_MediaPipe.py
@@ -0,0 +1,45 @@
+import cv2
+import numpy as np
+import os
+import time
+import mediapipe as mp
+from matplotlib import pyplot as plt
+
+mp_holistic = mp.solutions.holistic  ## Holistic model--to make out the detections
+mp_drawing = mp.solutions.drawing_utils  ## Drawing utilities--to draw out the detections
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) ## color conversion
+    image.flags.writeable = False   ## image is no longer writeable here
+    results = model.process(image)   ## Make prediction
+    image.flags.writeable = True   ## image is now writeable
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) ## color conversion
+    return image, results
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION)  ## Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS)  ## Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  ##Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS)  ## Draw right hand connections
+def draw_styled_landmarks(image,results):
+    #Draw face connections
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACEMESH_TESSELATION,
+                             mp_drawing.DrawingSpec(color=(80,110,10), thickness=1, circle_radius=1),
+                             mp_drawing.DrawingSpec(color=(80,256,121),thickness=1)
+                             )
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(80,44,121),thickness=2)
+                             )
+    #Draw left-right connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(121,44,250),thickness=2)
+                             )
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4),
+                             mp_drawing.DrawingSpec(color=(245,66,230),thickness=2)
+                             )
+
+
+
